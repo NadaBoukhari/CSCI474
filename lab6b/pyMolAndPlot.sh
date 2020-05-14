2ae9c16a8cf04f3f6d81649159f7ee9c33476db7
Nada Boukhari <boukhan@linux-01.cs.wwu.edu>
2020-05-14 15:43:14 -0700

    constructed plot

--- lab6b/pyMolAndPlot.sh
+++ lab6b/pyMolAndPlot.sh
@@ -26,3 +26,56 @@ done
 
 #invoke pymol
 pymol pymolCmnds.pml -qc
+
+# retrieve the CA atom IDs of the residues 25 and 51 of chain A of all structures
+for i in `seq 0 7`;
+do
+ aa25x[$i]=`grep "A  25" ${pdbIDs[$i]}_aligned.pdb | grep "ATOM" | grep "CA" | head -1 | awk '{print $7}'`
+ aa25y[$i]=`grep "A  25" ${pdbIDs[$i]}_aligned.pdb | grep "ATOM" | grep "CA" | head -1 | awk '{print $8}'`
+ aa25z[$i]=`grep "A  25" ${pdbIDs[$i]}_aligned.pdb | grep "ATOM" | grep "CA" | head -1 | awk '{print $9}'`
+
+ aa51x[$i]=`grep "A  51" ${pdbIDs[$i]}_aligned.pdb | grep "ATOM" | grep "CA" | head -1 | awk '{print $7}'`
+ aa51y[$i]=`grep "A  51" ${pdbIDs[$i]}_aligned.pdb | grep "ATOM" | grep "CA" | head -1 | awk '{print $8}'`
+ aa51z[$i]=`grep "A  51" ${pdbIDs[$i]}_aligned.pdb | grep "ATOM" | grep "CA" | head -1 | awk '{print $9}'`
+done
+
+# peform flap/active site distance calculations
+for i in `seq 0 7`;
+do
+ d=`echo "(${aa51x[$i]} - ${aa25x[$i]})^2+ (${aa51y[$i]} - ${aa25y[$i]})^2+(${aa51z[$i]} - ${aa25z[$i]})^2" | bc`
+ distance[$i]=`echo "sqrt($d)" | bc`
+done
+
+echo ${distance[@]}
+
+# make gnuplot data file
+rm gnuplotData.txt
+for i in `seq 0 7`;
+do
+	echo "$((i+1)) ${distance[$i]}" >> gnuplotData.txt
+done
+
+# gnuplot commands for terminal, x and y ranges, labels, title, etc.
+echo "set term png" > gnuplot.gpl
+echo "set output 'flapCatSitePlot.png'" >> gnuplot.gpl
+echo "set xrange[0:9]" >> gnuplot.gpl
+
+# to-do : Add an X label, Y label, and Title for the plot
+set title "Flap-catalytic site separation distance, 8 HIV-1 protease variants"
+set ylabel "Separation Distance (Ansgstroms)"
+set xlabel "Structure"
+
+
+# make xtic labels and generate the plot, and invoke gnuplot
+xticsString="("
+for i in `seq 0 7`;
+do
+	xticsString+="'${pdbIDs[$i]}' $((i+1)),"
+done
+
+xticsString+="'${pdbIDs[7]}' $((i+1)))"
+echo "set xtics $xticsString" >> gnuplot.gpl
+echo "plot 'gnuplotData.txt' using 1:2 with lines title 'sep distance'" >> gnuplot.gpl
+gnuplot gnuplot.gpl 
+
+
