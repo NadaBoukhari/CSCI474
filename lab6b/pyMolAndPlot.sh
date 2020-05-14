8f76c7b5161e8bc0e5c83c3068b4bd84d68197c2
Nada Boukhari <boukhan@linux-01.cs.wwu.edu>
2020-05-14 14:47:02 -0700

    fetched pdb files and aligned them

--- lab6b/pyMolAndPlot.sh
+++ lab6b/pyMolAndPlot.sh
@@ -1,1 +1,28 @@
-dazdazd
+#!/bin/bash
+
+# declare arrays of PDBs
+pdbIDs=('5con' '5coo' '5cop' '4hla' '5bry' '5bs4' '4hdp' '5dgw')
+target="5dgu"
+echo $target
+echo ${pdbIDs[@]}
+pdbLen=${#pdbIDs[@]}
+echo $pdbLen
+
+# fetch the target pdbID
+echo "fetch 5dgu, async=0" > pymolCmnds.pml
+
+# fetch all of the drug candidate structures
+for pdbID in "${pdbIDs[@]}"
+do
+	echo "fetch $pdbID, async=0" >> pymolCmnds.pml
+done
+
+# structurally align each candidate structure against the target
+for pdbID in "${pdbIDs[@]}"
+do
+	echo "align $pdbID & res 1-40+60-90 & name n+ca+c+o, $target & res 1-40+60-90 & name n+ca+c+o" >> pymolCmnds.pml
+	echo "save ${pdbID}_aligned.pdb, $pdbID" >> pymolCmnds.pml
+done
+
+#invoke pymol
+pymol pymolCmnds.pml -qc
